42ab95f61fafeba3e93633655565e81ecc0a7c7a
endowdly <endowdly@gmail.com>
2019-11-29 22:33:20 -0500

    Done GetStuffDone Needs Demos

--- GetStuffDone/convert.bat
+++ GetStuffDone/convert.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-START powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "& ./convert.ps1 presentation.adoc"
+START powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File convert.ps1 "presentation.adoc"
