4404aae443bb3e98f27853b7ecba892eb5b99cbc
endowdly <endowdly@gmail.com>
2019-12-01 21:33:53 -0500

    Fix
    Cleaning up some small errors and attempting to help a page build

--- GetStuffDone/demo/constants.cs
+++ GetStuffDone/demo/constants.cs
@@ -2,7 +2,7 @@ namespace EWSGDemo
 { 
     public static class Constants
     {
-        const double Tau = 2 * [Math]::PI; 
+        const double Tau = 2 * Math::PI; 
         const double SpeedOfLight = 2.99792458E8; 
         const double MagneticPermeability = 1.2566370614359172954E-6; 
         const double ElectricPermittivity = 1.2566370614359172954E-12; 
